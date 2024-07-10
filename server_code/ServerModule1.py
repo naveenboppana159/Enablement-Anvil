1c2295b1d4d3f906b7bb5b9ca4e74215a18741a1
ygnanendraprasad@gmail.com <ygnanendraprasad@gmail.com>
2024-07-10 13:17:37 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -26,7 +26,6 @@ def submit(full_name ,email_user,user_phonenumber,user_password,reenter_password
   app_tables.users.add_row(full_name=full_name, email_user = email_user, user_phonenumber=  user_phonenumber,user_password = user_password,reenter_password = reenter_password,user_type='employee')
 
 def hash_password(password):
-    """Hash a password for storing."""
     salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
     pwdhash = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'), salt, 100000)
     pwdhash = binascii.hexlify(pwdhash)
@@ -34,10 +33,7 @@ def hash_password(password):
 
 @anvil.server.callable
 def submit_user(full_name, email_user, user_phonenumber, user_password):
-    # Encrypt the password
     hashed_password = hash_password(user_password)
-    
-    # Store the data in the database
     app_tables.users.add_row(
         full_name=full_name,
         email_user=email_user,
@@ -47,14 +43,4 @@ def submit_user(full_name, email_user, user_phonenumber, user_password):
 
 @anvil.server.callable
 def get_users():
-    rows = app_tables.users.search()
-    users = []
-    for row in rows:
-        # For display purposes, we will not decrypt the password but show a fixed string of dots
-        users.append({
-            'full_name': row['full_name'],
-            'email_user': row['email_user'],
-            'user_phonenumber': row['user_phonenumber'],
-            'user_password': '•' * 8  # Display 8 dots for the password
-        })
-    return users
+    return [{'full_name': row['full_name'], 'email_user': row['email_user'], 'user_phonenumber': row['user_phonenumber'], 'user_password': '•' * 8} for row in app_tables.users.search()]

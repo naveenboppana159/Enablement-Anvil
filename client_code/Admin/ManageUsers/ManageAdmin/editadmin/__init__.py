6d21faeb8fff958b752185690f7511d0fd968db9
deepanshukori21@gmail.com <deepanshukori21@gmail.com>
2024-07-10 17:53:17 +0000

    Edited Admin.ManageUsers.ManageAdmin.editadmin; Deleted Admin.ManageUsers.ManageAdmin.editadmin.ItemTemplate5, Admin.ManageUsers.ManageAdmin.editadmin.ItemTemplate4, Admin.ManageUsers.ManageAdmin.editadmin.ItemTemplate3 and Admin.ManageUsers.ManageAdmin.editadmin.Form1

--- client_code/Admin/ManageUsers/ManageAdmin/editadmin/__init__.py
+++ client_code/Admin/ManageUsers/ManageAdmin/editadmin/__init__.py
@@ -1,19 +1,25 @@
 from ._anvil_designer import editadminTemplate
 from anvil import *
 import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 
 class editadmin(editadminTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.repeating_panel_1.items = app_tables.admin.search(usertype='admin')
+    self.repeating_panel_1.items = app_tables.users.search(user_type='admin')
 
     # Any code you write here will run before the form opens.
+  def link_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('Admin.ManageUsers.ManageAdmin')
 
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
-    open_form('Admin.ManageUsers.ManageAdmin.editadmin')
+    alert('Your Details Has Been Saved')
     
 
   def button_2_click(self, **event_args):
@@ -21,9 +27,7 @@ class editadmin(editadminTemplate):
     self.item.delete()
     self.remove_from_parent()
 
-  def link_1_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    open_form('Admin.ManageUsers.ManageAdmin')
+  
   
     
     

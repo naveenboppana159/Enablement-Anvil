027b7482ab856773ed9e09c7dbf762705c06737c
deepanshukori21@gmail.com <deepanshukori21@gmail.com>
2024-07-10 18:24:01 +0000

    Created Admin.ManageUsers.ManageAdmin.editadmin.ItemTemplate7 and Admin.ManageUsers.ManageAdmin.editadmin.ItemTemplate7.ItemTemplate2; Deleted Admin.ManageUsers.ManageAdmin.editadmin.ItemTemplate2

--- client_code/Admin/ManageUsers/ManageAdmin/editadmin/ItemTemplate7/ItemTemplate2/__init__.py
+++ client_code/Admin/ManageUsers/ManageAdmin/editadmin/ItemTemplate7/ItemTemplate2/__init__.py
@@ -1,6 +1,9 @@
 from ._anvil_designer import ItemTemplate2Template
 from anvil import *
 import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 
 class ItemTemplate2(ItemTemplate2Template):
---
+++ client_code/Admin/ManageUsers/ManageAdmin/editadmin/ItemTemplate7/__init__.py
@@ -0,0 +1,24 @@
+from ._anvil_designer import ItemTemplate7Template
+from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class ItemTemplate7(ItemTemplate7Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    self.repeating_panel_1.items = app_tables.users.search(user_type='admin')
+
+    # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    alert('Your Details Has Been Saved')
+
+  def button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.item.delete()
+    self.remove_from_parent()
